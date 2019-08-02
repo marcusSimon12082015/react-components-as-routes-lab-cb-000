f6a3b2f558bfdc107996c0d9eaeaad9ad77646e3
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-08-02 08:30:53 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,10 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie,i) => {
+          
+        }}
     </div>
   );
 };
