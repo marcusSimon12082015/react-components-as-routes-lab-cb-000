cfebaa101b0625951b654860f98d896007da2b34
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-08-02 10:28:24 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -5,7 +5,21 @@ const Directors = () => {
   return (
     <div>
       <h1>Directors Page</h1>
-
+      {
+        directors.map((director,i) => (
+          <div key={i}>
+          <p>Name: {director.name}</p>
+          <p>Movies:</p>
+          <ul>
+            {
+              director.movies.map((movie,j) => (
+                <li key={j}>{movie}</li>
+              ))
+            }
+          </ul>
+          </div>
+        ))
+      }
     </div>
   );
 }
