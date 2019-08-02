2dc8557a390bb75725d721a7287b8d77c3e43fb9
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-08-02 10:29:39 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,18 +5,20 @@ const Movies = () => {
   return (
     <div>
         <h1>Movies Page</h1>
-        {movies.map((movie,i) => {
+        {movies.map((movie,i) => (
           <div key={i}>
           <h2>Name: {movie.title}</h2>
           <p>Time: {movie.time}</p>
           <p>Genres:</p>
           <ul>
-            {movie.genres.map((genre,j) => {
+            {movie.genres.map((genre,j) => (
               <li key={j}>{genre}</li>
-            })}
+            ))
+          }
           </ul>
           </div>
-        })}
+          ))
+        }
     </div>
   );
 };
